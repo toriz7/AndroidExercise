9202b3fca919a7feba6dd559f30d23430df5842a
toriz7 <30500848+toriz7@users.noreply.github.com>
2021-04-04 14:03:21 +0900

    'test'

---
+++ app/src/main/java/kr/co/sony/androidexercise/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.sony.androidexercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
