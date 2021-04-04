9041accf51bdcbef485cd19132bacdd9aa6057ae
toriz7 <30500848+toriz7@users.noreply.github.com>
2021-04-05 00:42:39 +0900

    change env

--- app/src/main/java/kr/co/sony/androidexercise/MainActivity.kt
+++ app/src/main/java/kr/co/sony/androidexercise/MainActivity.kt
@@ -2,10 +2,18 @@ package kr.co.sony.androidexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.sony.androidexercise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view=binding.root
+        //setContentView(R.layout.activity_main)
+        setContentView(view)
+        binding.textView.text="Hello Software World!"
+        binding.toggleButton.textOff = "퇴직"
+        binding.toggleButton.textOn ="입사"
     }
 }
